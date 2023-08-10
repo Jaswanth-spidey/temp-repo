78d19e2482599d288528553da221ea665222cc8e
Jaswanth-Manda <107203674+Jaswanth-Manda@users.noreply.github.com>
2023-08-10 22:14:10 +0530

    In Pro: delete post front end

--- react-tn/blog/src/screens/IndexScreen.js
+++ react-tn/blog/src/screens/IndexScreen.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { EvilIcons } from '@expo/vector-icons'; 
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+
 
 const IndexScreen = () => {
+    const {state, addBlogPost} = useContext(Context);
+
     return (
         <View>
-            <Text>IndexScreen!!</Text>
+            <Button title="Add Post" onPress={addBlogPost}/>
+            <FlatList
+                data={state}
+                keyExtractor={(blogPosts) => blogPosts.title}
+                renderItem={({ item }) => {
+                    return <View style={styles.row}>
+                        <Text style={styles.title}>{item.title}</Text> 
+                        <EvilIcons name='trash' style={styles.icon}/>
+                    </View>;
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+
+    title: {
+        fontSize: 18
+    },
+
+    icon: {
+        fontSize: 14
+    }
+    
+});
 
 export default IndexScreen;
