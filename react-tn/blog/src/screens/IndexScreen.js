149a7a54e244e2064c6efd38f545921660325655
Jaswanth-Manda <107203674+Jaswanth-Manda@users.noreply.github.com>
2023-08-15 19:42:55 +0530

    updated

--- react-tn/blog/src/screens/IndexScreen.js
+++ react-tn/blog/src/screens/IndexScreen.js
@@ -1,49 +1,68 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import { Context } from '../context/BlogContext';
-import { EvilIcons } from '@expo/vector-icons'; 
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { Context } from "../context/BlogContext";
+import { EvilIcons, Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 // import { createAppContainer } from 'react-navigation';
 // import { createStackNavigator } from 'react-navigation-stack';
 
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
-const IndexScreen = () => {
-    const {state, addBlogPost} = useContext(Context);
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPosts) => blogPosts.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <EvilIcons name="trash" style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
 
-    return (
-        <View>
-            <Button title="Add Post" onPress={addBlogPost}/>
-            <FlatList
-                data={state}
-                keyExtractor={(blogPosts) => blogPosts.title}
-                renderItem={({ item }) => {
-                    return <View style={styles.row}>
-                        <Text style={styles.title}>{item.title}</Text> 
-                        <EvilIcons name='trash' style={styles.icon}/>
-                    </View>;
-                }}
-            />
-        </View>
-    );
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Feather name="plus" size={30} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingVertical: 20,
-        paddingHorizontal: 10,
-        borderTopWidth: 1,
-        borderColor: 'gray'
-    },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
 
-    title: {
-        fontSize: 18
-    },
+  title: {
+    fontSize: 18,
+  },
 
-    icon: {
-        fontSize: 14
-    }
-    
+  icon: {
+    fontSize: 14,
+  },
 });
 
 export default IndexScreen;
